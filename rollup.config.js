3d5ab46642445bd4de7e10ac86959ab29e99f0ec
Paco van der Linden <pavadeli@gmail.com>
2017-11-24 14:52:52 +0100

    fix rollup issues that results in unusable UMD build (#15)
    * add node globals to UMD version
    
    * 1.0.0-beta.2
    
    * changed plugin order to remove requires
    
    * 1.0.0-beta.3

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,30 @@
-import builtins from 'rollup-plugin-node-builtins';
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
+import resolve from 'rollup-plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import pkg from './package.json';
 
 export default [
-    // browser-friendly UMD build
+    // browser-friendly UMD build, mainly for StackBlitz support.
     {
         input: 'dist/index.js',
         output: {
             file: pkg.browser,
             format: 'umd'
         },
-        name: '@politie/informant',
+        name: 'informant',
         sourcemap: true,
         plugins: [
             sourcemaps(),
-            builtins(),
-            resolve(),
             commonjs({
                 namedExports: {
                     'verror': ['errorForEach', 'errorFromList', 'info', 'fullStack', 'hasCauseWithName', 'findCauseByName', 'VError']
                 }
-            })
+            }),
+            globals(),
+            builtins(),
+            resolve(),
         ],
     },
 
@@ -37,12 +39,12 @@ export default [
         sourcemap: true,
         plugins: [
             sourcemaps(),
-            resolve(),
             commonjs({
                 namedExports: {
                     'verror': ['errorForEach', 'errorFromList', 'info', 'fullStack', 'hasCauseWithName', 'findCauseByName', 'VError']
                 }
-            })
+            }),
+            resolve(),
         ],
     }
 ];
